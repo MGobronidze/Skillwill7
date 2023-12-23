158be5a5a30daf79d987d2049d53406e6b8fefda
MGobronidze <90101429+MGobronidze@users.noreply.github.com>
2023-12-23 22:39:54 +0400

    initial commit

---
+++ main.js
@@ -0,0 +1,56 @@
+// then catch
+function makeToys(){
+    return new Promise((resolve, reject)=>{
+        if(Math.random()>0.1){
+            resolve("Undefected")
+        }else{
+            reject('Defected')
+        }
+    })
+}
+
+
+function sellToys(status){
+    return new Promise((resolve, reject)=>{
+        if(status === 'Undefected'){
+            if(Math.random()>0.7){
+                resolve('Toy has been sold')
+            }else{
+                reject('Toy was unseccessful')
+            }
+        }
+    })
+}
+
+makeToys()
+    .then((status)=>sellToys(status))
+    .then((res)=>console.log(res))
+    .catch((error)=> console.log(error))
+
+
+// async awayt
+async function makeToysVol2(){
+        if(Math.random()>0.1){
+            return "Undefected"
+        }else{
+            return 'Defected'
+        }
+}
+async function sellToysVol2(status){
+        if(status === 'Undefected'){
+            if(Math.random()>0.7){
+                return 'Toy has been sold'
+            }else{
+                return 'Toy was unseccessful'
+            }
+        }
+}
+async function promisify(){
+    try{
+        const status = await makeToysVol2()
+        const result = await sellToysVol2(status)
+        console.log(result)
+    }catch(error){
+        console.log(error)
+    }
+}
